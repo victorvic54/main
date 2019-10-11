63846afee26943d839b365f12f476457b3416c45
Sacchirro Victor <sacchirro@yahoo.com>
2019-10-12 02:25:19 +0800

    Original Code for preference

---
+++ src/test/java/seedu/address/model/quiz/UserPrefsTest.java
@@ -0,0 +1,22 @@
+package seedu.address.model.quiz;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.UserPrefs;
+
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class UserPrefsTest {
+
+    @Test
+    public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
+        UserPrefs userPref = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+    }
+
+    @Test
+    public void setAddressBookFilePath_nullPath_throwsNullPointerException() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setAddressBookFilePath(null));
+    }
+
+}
